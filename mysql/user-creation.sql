c8a6ace3f03ee313f69abf5aa5601882a7b442b8
Lois Onyinyemme Bassey <45472714+ThatLadyDev@users.noreply.github.com>
2024-11-15 10:57:41 +0100

    removal of files

--- mysql/user-creation.sql
+++
@@ -1,8 +0,0 @@
--- Create the user (if it doesn't exist)
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-
--- Grant the necessary privileges on the databases
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
-
--- Apply the changes
-FLUSH PRIVILEGES;
